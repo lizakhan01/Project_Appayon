a9ede470508405b9c103dee35f39090c3037c5a8
Khadiza Khanom <lk221201@gmail.com>
2024-06-29 19:42:35 +0600

    Added SignIn and SignUp page.

--- appayon/src/App.jsx
+++ appayon/src/App.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Components/Home";
+import SignUp from "./Components/SignUp";
+import SignIn from "./Components/SignIn";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-        <Route path="/home" element={<Home />} />
-        
-
+          <Route path="/home" element={<Home />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/signIn" element={<SignIn />} />
         </Routes>
       </Router>
-      
     </div>
   );
 }
