fcec64a6f0a87930d5c08214fb68b21bda7d7b5e
Sonod Sadman <sonodsadman@gmail.com>
2024-07-17 03:07:38 +0600

    Fix3

--- appayon/src/Components/LoginPopup/LoginPopup.jsx
+++ appayon/src/Components/LoginPopup/LoginPopup.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useContext, useState } from "react";
-import { assets } from "../../assets/assets";
-//import { assets } from "../../Assets/assets";
+//import { assets } from "../../assets/assets";
+import { assets } from "../../Assets/assets";
 import { StoreContext } from "../../context/StoreContext";
 import "./LoginPopup.css";
 
