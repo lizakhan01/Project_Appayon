912d7472cadf122dec578c0273ff7ba895bd981c
Sonod Sadman <sonodsadman@gmail.com>
2024-08-09 15:06:15 +0600

    Initial commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Best place to find your favourite food</h2>
+        <p>Experience a world of flavors with our exquisite dishes, crafted by talented chefs and delivered straight to your door. Enjoy an unforgettable dining experience at home with the freshest ingredients and a passion for perfection, exclusively through our online delivery service.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
