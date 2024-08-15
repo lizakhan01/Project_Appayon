6917de3969ba768d60776a94c98f82ef11c30031
Sonod Sadman <sonodsadman@gmail.com>
2024-08-15 22:41:51 +0600

    Cart,Order_Frontend

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className='header'>
       <div className="header-contents">
-        <h2>Best place to find your favourite food</h2>
+        <h2>Get Your Favorite Food, Fast and Fresh!</h2>
         <p>Experience a world of flavors with our exquisite dishes, crafted by talented chefs and delivered straight to your door. Enjoy an unforgettable dining experience at home with the freshest ingredients and a passion for perfection, exclusively through our online delivery service.</p>
         <button>View Menu</button>
       </div>
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,12 +1,77 @@
-import React from 'react'
-import './Cart.css'
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { StoreContext } from "../../context/StoreContext";
+import "./Cart.css";
 
 const Cart = () => {
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount } = useContext(StoreContext);
+  const navigate = useNavigate();
   return (
-    <div>
-      
+    <div className="cart">
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div>
+                <div className="cart-items-title cart-items-item">
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>৳{item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>৳{item.price * cartItems[item._id]}</p>
+                  <p onClick={() => removeFromCart(item._id)} className="cross">
+                    X
+                  </p>
+                </div>
+                <hr />
+              </div>
+            );
+          }
+        })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>৳{getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Charge</p>
+              <p>৳{getTotalCartAmount()===0?0:50}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>৳{getTotalCartAmount()===0?0:getTotalCartAmount()+50}</b>
+            </div>
+          </div>
+          <button onClick={() => navigate("/order")}>Proceed to Checkout</button>
+        </div>
+        <div className="cart-promocode">
+          <div>
+            <p>Apply Promocode, if any</p>
+            <div className="cart-promocode-input">
+              <input type="text" placeholder="Promocode" />
+              <button>Apply</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
