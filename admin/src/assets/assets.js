056e4ca03795f0539ed83fec41da2e3da97a6f99
Sonod Sadman <sonodsadman@gmail.com>
2024-09-19 18:24:49 +0600

    Vercel

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,9 +1,9 @@
-import logo from './logo.png'
 import add_icon from './add_icon.png'
+import logo from './logo.png'
 import order_icon from './order_icon.png'
+import parcel_icon from './parcel_icon.png'
 import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
-import parcel_icon from './parcel_icon.png'
 
 export const assets ={
     logo,
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'http://localhost:4000'
+export const url = 'https://appayon-server.vercel.app'
