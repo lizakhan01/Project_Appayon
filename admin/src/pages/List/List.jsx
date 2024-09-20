dc4a651a6d41ae723874a26209f781f718c11f13
Sonod Sadman <sonodsadman@gmail.com>
2024-09-20 23:51:46 +0600

    img_prob_solved

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -47,7 +47,7 @@ useEffect(() => {
         {list.map((item,index)=>{
           return (
             <div key={index} className='list-table-format'> 
-            <img src={`${url}/images/`+item.image} alt="" />
+            <img src={item.image} alt="" />
             <p>{item.name}</p>
             <p>{item.category}</p>
             <p>৳{item.price}</p>
