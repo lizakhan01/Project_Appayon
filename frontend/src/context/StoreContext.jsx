cf41b0c52cd7e75bf0743509e16360b601760cbd
Sonod Sadman <sonodsadman@gmail.com>
2024-09-21 21:21:20 +0600

    test3

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "https://appayonserver.vercel.app";	
+  const url = "https://appayon-server-jade.vercel.app";	
   const [token,setToken] = useState("");
   const [food_list,setFoodList] = useState([]);
 
